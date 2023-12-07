be1a4c9c53299dae5403b304d8a4c2b5f5f5b8da
hongjfix <gaohongj@outlook.com>
2023-12-07 01:39:48 -0800

    init

---
+++ src/main/java/com/hackathon/recengine/RecEngineApplication.java
@@ -0,0 +1,14 @@
+package com.hackathon.recengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecEngineApplication.class, args);
+		System.out.println("hello");
+	}
+
+}
